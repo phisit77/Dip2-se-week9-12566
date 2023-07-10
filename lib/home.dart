9a630032d32e06779c06ed42bf50c2a1fcd7c120
phisit77 <kachidoki787@gmail.com>
2023-07-10 15:51:38 +0700

    first app

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+class  Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+    title: Text("Home"),
+),
+body: Center(
+child: Text("MyApp"),
+),
+floatingActionButton: FloatingActionButton(
+onPressed: () {
+print("OK");
+},
+child: Icon(Icons.train),
+),
+);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); //1.constructor
+
+  @override //2.build
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:
+    );
+  }
+}
